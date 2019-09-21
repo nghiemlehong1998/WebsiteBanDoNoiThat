7a1e23770251a03aa2518cfe8bc6fe88403bdaf6
Le Hong Nghiem <nghiemlehong98@gmail.com>
2019-09-22 00:03:28 +0700

    add database

---
+++ BanDoNoiThat.sql
@@ -0,0 +1,67 @@
+﻿CREATE DATABASE WebsiteBanDoNoiThat
+USE WebsiteBanDoNoiThat
+GO
+--Bảng danh mục
+CREATE TABLE DanhMucSanPham(
+MaDanhMuc INT IDENTITY PRIMARY KEY,
+TenDanhMuc NVARCHAR(100))
+--Bảng sản phẩm 
+CREATE TABLE SanPham(
+MaSanPham INT IDENTITY PRIMARY KEY,
+MaDanhMuc INT FOREIGN KEY REFERENCES dbo.DanhMucSanPham(MaDanhMuc),
+TenSanPham NVARCHAR(100),
+GiaGoc FLOAT,
+KhuyenMai FLOAT,
+MoTaNgan NVARCHAR(200),
+MoTaChiTiet NTEXT
+)
+--Bảng tin tức
+CREATE TABLE TinTuc(
+MaTinTuc INT IDENTITY PRIMARY KEY,
+TieuDe NVARCHAR(200),
+NoiDung NTEXT)
+--Bảng Liên hệ
+CREATE TABLE LienHe(
+MaLienHe INT IDENTITY PRIMARY KEY,
+HoTen NVARCHAR(50),
+DiaChi NVARCHAR(100),
+Email NVARCHAR(100),
+DienThoai NVARCHAR(20),
+TieuDe NVARCHAR(200),
+NoiDung NTEXT)
+--Bảng Users
+CREATE TABLE Users(
+TaiKhoan NVARCHAR(100) NOT NULL PRIMARY KEY,
+MatKhau NVARCHAR(100) NOT NULL,
+Email NVARCHAR(100),
+HoTen NVARCHAR(100,
+GioiTinh BIT,
+NgaySinh DATETIME,
+DienThoai INT,
+DiaChi NVARCHAR(200),
+TinhTP NVARCHAR(100),
+QuanHuyen NVARCHAR(100),
+NhomQuyen INT NOT NULL--0: admin, 1: Nhân viên 2: Khách hàng
+)
+--Bảng đơn hàng
+
+CREATE TABLE DonHang(
+MaDonHang INT IDENTITY PRIMARY KEY,
+NgayDatHang DATETIME,
+HoTen NVARCHAR(100),
+DiaChi NVARCHAR(200),
+Email NVARCHAR(100)
+DienThoai INT,
+SoLuongSanPham INT,
+TongTien FLOAT)
+
+--Đơn hàng chi tiết
+
+CREATE TABLE DonHangChiTiet(
+MaDonHangChiTiet INT IDENTITY PRIMARY KEY,
+MaDonHang INT FOREIGN KEY REFERENCES dbo.DonHang(MaDonHang),
+MaSanPham INT FOREIGN KEY REFERENCES dbo.SanPham(MaSanPham),
+GiaBan FLOAT,
+SoLuongSanPhan INT,
+SoTien FLOAT)
+
