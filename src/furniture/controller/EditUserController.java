16ba72744a3001a8771b5d8f16d0780cde213802
Le Hong Nghiem <nghiemlehong98@gmail.com>
2019-10-05 21:57:51 +0700

    edit user

--- src/furniture/controller/EditUserController.java
+++ src/furniture/controller/EditUserController.java
@@ -1,6 +1,5 @@
 package furniture.controller;
 
-
 import java.util.List;
 
 import javax.servlet.http.HttpServletRequest;
@@ -17,48 +16,50 @@ import org.springframework.web.bind.annotation.RequestMethod;
 
 import furniture.enity.Users;
 
-
 @Transactional
 @Controller
 
 public class EditUserController {
 	@Autowired
 	SessionFactory factory;
-	
-	@RequestMapping(value = "admin/edit-user",method = RequestMethod.POST)
-	public String edit(HttpServletRequest request, ModelMap model)
-	{
+
+	@RequestMapping(value = "admin/edit-user", method = RequestMethod.POST)
+	public String edit(HttpServletRequest request, ModelMap model) {
 		String id = request.getParameter("btnEdit");
 		Session session = factory.getCurrentSession();
-		String hql ="FROM Users";
+		String hql = "FROM Users";
 		Query query = session.createQuery(hql);
-		List <Users> list = query.list();
-		for(Users a : list)
-		{
-			if(a.getMaTaiKhoan() == Integer.parseInt(id)) model.addAttribute("user", a);
+		List<Users> list = query.list();
+		for (Users a : list) {
+			if (a.getMaTaiKhoan() == Integer.parseInt(id))
+				model.addAttribute("user", a);
 		}
 		return "/admin/edit-user";
 	}
-	
+
 	@RequestMapping(value = "admin/users", method = RequestMethod.POST)
-	public String edit2(HttpServletRequest request, ModelMap model)
-	{
+	public String edit2(HttpServletRequest request, ModelMap model) {
 		String maTaiKhoan = request.getParameter("btnSaveChange");
 		String hoTen = request.getParameter("hoTen");
 		String gioiTinh = request.getParameter("gioiTinh");
-		System.out.println(gioiTinh);
+		boolean gioiTinh2;
+		if(gioiTinh.equals("Nam")==true)gioiTinh2 = true;else gioiTinh2= false;		
 		String diaChi = request.getParameter("diaChi");
 		String email = request.getParameter("email");
 		String dienThoai = request.getParameter("dienThoai");
 		Session session = factory.getCurrentSession();
-		String hql ="update Users u set u.hoTen = '"+hoTen +"' where u.maTaiKhoan ="+ Integer.parseInt(maTaiKhoan) ;
+		String hql = "UPDATE Users u set u.hoTen = '"+hoTen+"',u.gioiTinh ="+gioiTinh2+",u.diaChi ='"+diaChi+"',u.email ='"+email+"',u.dienThoai ='"+dienThoai+"' WHERE u.maTaiKhoan = " + maTaiKhoan;
 		Query query = session.createQuery(hql);
-		String hql2 ="FROM Users";
-		Query query2 = session.createQuery(hql);
-		List <Users> list = query2.list();
-		model.addAttribute("Users",list);
+//		query.setParameter("maTK",Integer.parseInt(maTaiKhoan));
+//		query.setParameter("hoTenUser", hoTen);
+		int result = query.executeUpdate();
+		System.out.println(hoTen);
+		System.out.println(maTaiKhoan);
+		String hql2 = "FROM Users";
+		Query query2 = session.createQuery(hql2);
+		List<Users> list = query2.list();
+		model.addAttribute("Users", list);
 		return "/admin/users";
 	}
 
-	
 }
